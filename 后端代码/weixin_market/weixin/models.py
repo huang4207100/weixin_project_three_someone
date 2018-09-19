e67235193bb1a9958587aceca114213993f39af5
ubuntu <ubuntu@localhost.localdomain>
2018-09-19 15:58:46 +0800

    update models

--- 后端代码/weixin_market/weixin/models.py
+++ 后端代码/weixin_market/weixin/models.py
@@ -11,8 +11,19 @@ class Account_info(models.Model):
     phone = models.CharField(max_length=20)             #账号的手机号
     password = models.CharField(max_length=50) #账户的密码
 
+class Coupon(models.Model):
+    weixin_user = models.ForeignKey(Weixin_user,on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)  #订单生成时间
+    price_choice = (
+        ("login","29-5"),
+        ("share_once","99-19"),
+        ("share_twice","149-39"),
+    )
+    price = models.CharField(max_length=20,choices=price_choice)  #3中订单的状态
+
+
 class Class_info(models.Model):
-    account_info = models.ForeignKey(Account_info,on_delete=models.CASCADE) #课程的账号
+    coupon_id = models.ForeignKey(Coupon,on_delete=models.CASCADE) #课程的账号
     class_name = models.CharField(max_length=50)    #课程的名字
     class_percent = models.IntegerField(default=0,validators=[MaxValueValidator(100),MinValueValidator(0)]) #课程的百分比
 
@@ -29,12 +40,11 @@ class Order_info(models.Model):
     price = models.FloatField(default=9.9)                                       #订单的最终价格
     generate_info = models.CharField(max_length=1000)                            #上传的刷课数据，后面用于导出
 
-class Coupon(models.Model):
-    weixin_user = models.ForeignKey(Weixin_user,on_delete=models.CASCADE)
-    start_time = models.DateTimeField(auto_now_add=True)  #订单生成时间
-    price_choice = (
-        ("login","29-5"),
-        ("share_once","99-19"),
-        ("share_twice","149-39"),
-    )
-    price = models.CharField(max_length=20,choices=price_choice)  #3中订单的状态
+
+# class Goods(models.Model):
+#     goods_name = models.CharField(max_length=50)
+#     goods_count = models.IntegerField(default=0)
+#
+# class Shopping_car(models.Model):
+#     user_id = models.ForeignKey(Weixin_user,on_delete=models.CASCADE)
+#     good_id
